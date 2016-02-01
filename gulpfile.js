7947e79d7e8b54cfde77d144e6be3f0b9583a311
kerrongordon <kgpsounds.com@gmail.com>
2016-01-31 20:25:50 -0400

    clean up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -18,117 +18,165 @@ var ghPages     = require('gulp-gh-pages');
 var sourcemaps  = require('gulp-sourcemaps');
 
 var messages = {
-    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+    jekyllBuild: '<span style="color: green">Running:</span> $ jekyll build'
 };
 
-/**
- * Build the Jekyll Site
- */
+var app = {
+  base:   'app',
+  sass:   'app/_scss/main.sass',
+  css:    'app/assets/css',
+  js:     'app/_script/**/*.js',
+  jsa:    'app/assets/js'
+};
+
+var site = {
+  base:   '_site',
+  css:    '_site/assets/css',
+  html:   '_site/**/*.html',
+  cssAll: '_site/assets/css/**/*.css',
+  img:    '_site/assets/img/**/*',
+  jsAll:  '_site/assets/js/**/*.js'
+};
+
+var dist = {
+  all:    'dist/**',
+  dist:   'dist',
+  css:    'dist/assets/css/',
+  img:    'dist/assets/img/',
+  js:     'dist/assets/js/'
+};
+
+var watch = {
+  sass:   'app/_scss/**/*.sass',
+  js:     'app/_script/**/*.js',
+  html:   'app/**/*.html',
+  post:   'app/_posts/*',
+  data:   'app/_data/*'
+};
+
+//--------------------------------------------------------------------------------------------------------
+// Build the Jekyll Site
+//--------------------------------------------------------------------------------------------------------
+
 gulp.task('jekyll-build', function (done) {
     browserSync.notify(messages.jekyllBuild);
     return cp.spawn('jekyll', ['build'], {stdio: 'inherit'})
         .on('close', done);
 });
 
-/**
- * Rebuild Jekyll & do page reload
- */
+
+//--------------------------------------------------------------------------------------------------------
+// Rebuild Jekyll & do page reload
+//--------------------------------------------------------------------------------------------------------
+
 gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
     browserSync.reload();
 });
 
-/**
- * Wait for jekyll-build, then launch the Server
- */
+
+//--------------------------------------------------------------------------------------------------------
+// Wait for jekyll-build, then launch the Server
+//--------------------------------------------------------------------------------------------------------
+
 gulp.task('browser-sync', ['sass', 'js', 'jekyll-build'], function() {
     browserSync({
         server: {
-            baseDir: '_site'
+            baseDir: site.base
         }
     });
 });
 
-/**
- * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
- */
+
+
+//--------------------------------------------------------------------------------------------------------
+// Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
+//--------------------------------------------------------------------------------------------------------
+
 gulp.task('sass', function () {
-    return gulp.src('app/_scss/main.sass')
+    return gulp.src(app.sass)
       .pipe(sourcemaps.init())
-        .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
-        .pipe(sass({
-            includePaths: ['scss'],
-            indentedSyntax: true,
-            onError: browserSync.notify
-        }))
+        .pipe(plumber({ errorHandler: notify.onError("Error: <%= error.message %>") }))
+        .pipe(sass({ includePaths: ['scss'], indentedSyntax: true, onError: browserSync.notify }))
         .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-        .pipe(gulp.dest('_site/assets/css'))
+        .pipe(gulp.dest(site.css))
         .pipe(browserSync.reload({stream:true}))
       .pipe(sourcemaps.write())
-      .pipe(gulp.dest('app/assets/css'));
+      .pipe(gulp.dest(app.css));
 });
 
-/**
- * Concat and uglify files from _script into assets/js
- */
+
+//--------------------------------------------------------------------------------------------------------
+// Concat and uglify files from _script into assets/js
+//--------------------------------------------------------------------------------------------------------
 
 gulp.task('js', function () {
-  return gulp.src('app/_script/**/*.js')
+  return gulp.src(app.js)
     .pipe(sourcemaps.init())
       .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
       .pipe(jshint())
       .pipe(jshint.reporter(stylish))
       .pipe(concat('main.js'))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('app/assets/js'));
+    .pipe(gulp.dest(app.jsa));
 });
 
-/**
- * Rmove old dist dir
- */
+
+//--------------------------------------------------------------------------------------------------------
+// Rmove old dist dir
+//--------------------------------------------------------------------------------------------------------
 
 gulp.task('clean', ['jekyll-build', 'js', 'sass'], function (cb) {
-  return del(['dist/**'], cb);
+  return del([dist.all], cb);
 });
 
-/**
- * Minify html and copy them into the dist dir
- */
+
+//--------------------------------------------------------------------------------------------------------
+// Minify html and copy them into the dist dir
+//--------------------------------------------------------------------------------------------------------
 
 gulp.task('htmlmin', ['clean'], function () {
-  return gulp.src('_site/**/*.html')
+  return gulp.src(site.html)
     .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest(dist.dist));
 });
 
-/**
- * Minify css and copy them into the dist dir
- */
+
+//--------------------------------------------------------------------------------------------------------
+// Minify css and copy them into the dist dir
+//--------------------------------------------------------------------------------------------------------
 
 gulp.task('cssmin', ['htmlmin'], function () {
-  return gulp.src('_site/assets/css/**/*.css')
+  return gulp.src(site.cssAll)
     .pipe(minifyCss())
-    .pipe(gulp.dest('dist/assets/css/'));
+    .pipe(gulp.dest(dist.css));
 });
 
-/**
- * copy images and minify them into the dist dir
- */
+
+//--------------------------------------------------------------------------------------------------------
+// copy images and minify them into the dist dir
+//--------------------------------------------------------------------------------------------------------
 
 gulp.task('imgmin', ['cssmin'], function () {
-  return gulp.src('_site/assets/img/**/*')
+  return gulp.src(site.img)
     .pipe(imagemin({ progressive: true }))
-    .pipe(gulp.dest('dist/assets/img/'));
+    .pipe(gulp.dest(dist.img));
 });
 
-/**
- * copy css and js form _site dir into the dist dir
- */
+
+//--------------------------------------------------------------------------------------------------------
+// copy css and js form _site dir into the dist dir
+//--------------------------------------------------------------------------------------------------------
 
 gulp.task('uglifyjs', ['imgmin'], function () {
-  return gulp.src('_site/assets/js/**/*.js')
+  return gulp.src(site.jsAll)
     .pipe(uglify())
-    .pipe(gulp.dest('dist/assets/js/'));
+    .pipe(gulp.dest(dist.js));
 });
+
+
+//--------------------------------------------------------------------------------------------------------
+// copy files form _site dir into the dist dir
+//--------------------------------------------------------------------------------------------------------
 
 gulp.task('copy', ['uglifyjs'], function () {
   var paths = [
@@ -137,9 +185,10 @@ gulp.task('copy', ['uglifyjs'], function () {
   return copy2(paths);
 });
 
-/**
- * build site into dist dir
- */
+
+//--------------------------------------------------------------------------------------------------------
+// build site into dist dir
+//--------------------------------------------------------------------------------------------------------
 
 gulp.task('build', ['copy'], function () {
   return gulp.src("_site/")
@@ -147,26 +196,32 @@ gulp.task('build', ['copy'], function () {
 });
 
 
+//--------------------------------------------------------------------------------------------------------
+// deploy site to github pages
+//--------------------------------------------------------------------------------------------------------
+
 gulp.task('deploy', ['build'], function () {
   return gulp.src('dist/**/*')
     .pipe(ghPages());
 });
 
 
-/**
- * Watch scss files for changes & recompile
- * Watch html/md files, run jekyll & reload BrowserSync
- */
+//--------------------------------------------------------------------------------------------------------
+// Watch scss files for changes & recompile
+// Watch html/md files, run jekyll & reload BrowserSync
+//--------------------------------------------------------------------------------------------------------
+
 gulp.task('watch', function () {
-    gulp.watch('app/_scss/**/*.sass', ['sass']);
-    gulp.watch('app/_script/**/*.js', ['js', 'jekyll-rebuild']);
-    gulp.watch(['app/**/*.html', 'app/_posts/*', 'app/_data/*'], ['jekyll-rebuild']);
+    gulp.watch(watch.sass, ['sass']);
+    gulp.watch(watch.js, ['js', 'jekyll-rebuild']);
+    gulp.watch([watch.html, watch.post, watch.data], ['jekyll-rebuild']);
 });
 
-/**
- * Default task, running just `gulp` will compile the sass,
- * compile the jekyll site, launch BrowserSync & watch files.
- */
-gulp.task('default', ['build']);
 
+//--------------------------------------------------------------------------------------------------------
+// Default task, running just `gulp serve` will compile the sass,
+// compile the jekyll site, launch BrowserSync & watch files.
+//--------------------------------------------------------------------------------------------------------
+
+gulp.task('default', ['build']);
 gulp.task('serve', ['browser-sync', 'watch']);
